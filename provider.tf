fe44f71625b20c32e2b68671470348edc6cf102b
TaosifAhmed88 <138560477+TaosifAhmed88@users.noreply.github.com>
2025-10-09 14:41:55 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,4 +1,6 @@
 provider "azurerm" {
     features {}
-    subscription_id = "5a0a3f52-2eea-4305-ba20-bca45b3303dc"
+    subscription_id = "28e1e42a-4438-4c30-9a5f-7d7b488fd883"
+    resource_provider_registrations = "none"
 }
+
